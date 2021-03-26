d2361e0ab7cd27042cfb450e4c2faa169404165e
adsoftsito <adsoft@live.com.mx>
2021-03-26 16:49:24 +0000

    add variables.py

---
+++ variables.py
@@ -0,0 +1,22 @@
+# variables.py
+# Author: Adolfo Centeno
+# March 26, 2021
+
+year = 2021;
+sentence = "My name is adsoft";
+sarah, bob, mike = 16, 20, 25;
+name, age = "maria", 24;
+mary = fer = 20;
+PI = 3.1416;
+e = 2.71;
+
+# display values
+
+print ("year = ", year);
+print (sentence);
+print ("sara : ", sarah, "\nbob : " , bob, "\nmike : ", mike);
+print ("\n\t name: ", name);
+print ("\n\t age : ", age);
+print (mary, fer);
+print (PI);
+print (e);
